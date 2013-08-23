cc258b2144ec8440fedfac5fdf03db5500f7bcaf
Michael Stenta <mike@mstenta.net>
2013-08-23 17:13:21 -0400

    Clean up old arguments.

--- manifests/dl.pp
+++ manifests/dl.pp
@@ -1,26 +1,21 @@
 define drush::dl (
   $type       = 'module',
-  $version    = '',
   $site_alias = $drush::defaults::site_alias,
   $options    = $drush::defaults::options,
   $arguments  = $drush::defaults::arguments,
-  $site_path  = $drush::defaults::site_path,
   $drush_user = $drush::defaults::drush_user,
   $drush_home = $drush::defaults::drush_home,
   $log        = $drush::defaults::log
   ) {
 
-  if $version { $real_ver = "-${version}"}
-
   if $arguments { $real_args = $arguments }
-  else { $real_args = "${name}${real_ver}" }
+  else { $real_args = "${name}" }
 
   drush::run {"drush-dl:${name}":
     command    => 'pm-download',
     site_alias => $site_alias,
     options    => $options,
     arguments  => $real_args,
-    site_path  => $site_path,
     drush_user => $drush_user,
     drush_home => $drush_home,
     log        => $log,
@@ -38,10 +33,6 @@ define drush::dl (
         unless => "[ -d '${drush_home}/.drush/${name}' ]",
       }
     }
-  }
-
-  if $site_path {
-    Drush::Run["drush-dl:${name}"] { creates => "${site_path}/${type}s/${name}"}
   }
 
   if defined(Drush::Run["drush-en:${name}"]) {
